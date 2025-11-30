26adcd35444daf4d7615974e5a766f013b4f1de6
ArshadShaik07 <arshadshaik2007@gmail.com>
2025-11-30 11:43:11 +0530

    updated experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,6 +2,9 @@ function Experience() {
 	return (
 		<section className="w-full py-14 px-6 flex justify-center font-mono">
 			<div className="max-w-3xl p-8 rounded-xl text-center flex flex-col items-start">
+				<p className="text-5xl font-semibold mb-4 text-[#f0f0f0]">
+					Expeience
+				</p>
 				<p className="text-3xl font-semibold mb-4 text-[#f5b95f]">
 					No Experience Yet ;)
 				</p>
